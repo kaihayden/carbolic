c8bed2c86d943da8ea3a7cbdfc58e7ba973969c9
kai1130 <kaihayden97@gmail.com>
2023-11-07 01:10:15 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,54 @@
+
+import streamlit as st
+
+import os
+import time
+from openai import OpenAI
+
+from carbolic import create_assistant, create_thread, send_message, generate_response
+
+client = OpenAI()
+OpenAI.api_key = st.secrets["OPENAI_API_KEY"]
+
+# - Params -------------------------------------------------------- #
+assistant_name = 'Carbolic'
+assistant_instructions = """
+You are a legal assistant chatbot. Use your knowledge to perform tasks such as proofreading and answer retrieval to help your solicitor perform their tasks to the highest quality.
+"""
+assistant_files=[]
+# ----------------------------------------------------------------- #
+
+st.title("Carbolic by Kai")
+
+if "model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4-1106-preview"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+assistant_files = st.file_uploader("Select Files", accept_multiple_files=True)
+assistant = create_assistant(client, name=assistant_name, instructions=assistant_instructions, files=assistant_files)
+
+thread = create_thread(client)
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Hey Carbolic, ..."):
+
+    message, log = send_message(client, thread, prompt)
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    st.session_state.messages.append(log)
+
+    with st.chat_message("assistant"):
+
+        run, log = generate_response(client, thread, assistant)
+
+        message_placeholder = st.empty()
+        message_placeholder.markdown(log['content'])
+
+    st.session_state.messages.append(log)
---
+++ carbolic.py
@@ -0,0 +1,77 @@
+def create_assistant(client, name, instructions, files=None):
+
+    assistant = client.beta.assistants.create(
+        name=name,
+        instructions=instructions,
+        model="gpt-4-1106-preview",
+        tools=[{"type": "retrieval"}],
+        file_ids=[prepare_file(f).id for f in files]
+    )
+    
+    return assistant
+
+def prepare_file(client, path):
+    file = client.files.create(
+        file=path,
+        purpose='assistants'
+    )
+    return file
+
+def create_thread(client):
+    thread = client.beta.threads.create()
+    
+    return thread
+
+def send_message(client, thread, user_message):
+    
+    message = client.beta.threads.messages.create(
+        thread_id = thread.id,
+        role="user",
+        content=user_message
+    )
+    
+    log = last_message(client, thread)
+    
+    return message, log
+
+def start_run(client, thread, assistant):
+    
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+        model="gpt-4-1106-preview",
+        tools=[{"type": "retrieval"}]
+    )
+    
+    return run
+
+def retrieve_run(client, thread, run):
+    
+    run = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+    )
+    
+    return run
+
+def generate_response(client, thread, assistant):
+    run = start_run(client, thread, assistant)
+
+    while run.status != 'completed':
+        run = retrieve_run(thread, run)
+        time.sleep(0.5)
+        
+    log = last_message(client, thread)
+        
+    return run, log
+
+def last_message(client, thread):
+    messages = client.beta.threads.messages.list(
+      thread_id=thread.id
+    )
+    last_msg = messages.dict()['data'][0]
+    
+    role = last_msg['role']
+    content = last_msg['content'][0]['text']['value']
+    
+    return {'role': role, 'content': content}
