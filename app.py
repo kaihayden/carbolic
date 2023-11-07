8712027f95418e262d101c041a4f57512e5cf78d
kai1130 <kaihayden97@gmail.com>
2023-11-07 01:56:01 -0800

    Update app.py

--- app.py
+++ app.py
@@ -95,3 +95,32 @@ if st.session_state["files_uploaded"]:
         st.session_state.messages.append(log)
 else:
     st.info("Please upload files or skip upload to proceed.")
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
