7efb92ee5fa2b96f57e43d94a697a28886115481
kai1130 <kaihayden97@gmail.com>
2023-11-07 01:53:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 
 import os
@@ -12,9 +11,9 @@ os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 OpenAI.api_key = os.environ["OPENAI_API_KEY"]
 
 # - Params -------------------------------------------------------- #
-assistant_name = 'Carbolic'
+assistant_name = "Carbolic"
 assistant_instructions = "You are a legal assistant chatbot. Use your knowledge to perform tasks such as proofreading and answer retrieval to help your solicitor perform their tasks to the highest quality."
-assistant_files=[]
+assistant_files = []
 # ----------------------------------------------------------------- #
 
 st.markdown(
@@ -22,7 +21,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-st.header("Carbolic", divider='rainbow')
+st.header("Carbolic", divider="rainbow")
 
 if "model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4-1106-preview"
@@ -30,44 +29,51 @@ if "model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-if 'files_uploaded' not in st.session_state:
-    st.session_state['files_uploaded'] = False
+if "files_uploaded" not in st.session_state:
+    st.session_state["files_uploaded"] = False
 
-if 'files' not in st.session_state:
-    st.session_state['files'] = []
+if "files" not in st.session_state:
+    st.session_state["files"] = []
 
-if 'instructions' not in st.session_state:
-    st.session_state['instructions'] = assistant_instructions
+if "instructions" not in st.session_state:
+    st.session_state["instructions"] = assistant_instructions
 
 if "configurator_state" not in st.session_state:
     st.session_state["configurator_state"] = True
 
+
 def toggle_closed():
     st.session_state["configurator_state"] = False
 
-with st.expander("Configurator", expanded=st.session_state['configurator_state']):
 
-    st.session_state['instructions'] = st.text_area(label='Instructions', value=st.session_state['instructions'])
+with st.expander("Configurator", expanded=st.session_state["configurator_state"]):
+    st.session_state["instructions"] = st.text_area(
+        label="Instructions", value=st.session_state["instructions"]
+    )
 
     assistant_files = st.file_uploader("Files", accept_multiple_files=True)
 
     if assistant_files:
-        st.session_state['files'] = assistant_files
+        st.session_state["files"] = assistant_files
 
-    if not st.session_state['files_uploaded'] and assistant_files:
-        if st.button('Confirm Files', on_click=toggle_closed):
-            st.session_state['files_uploaded'] = True
-            st.success('File Upload Success!')
+    if not st.session_state["files_uploaded"] and assistant_files:
+        if st.button("Confirm Files", on_click=toggle_closed):
+            st.session_state["files_uploaded"] = True
+            st.success("File Upload Success!")
 
-    if not st.session_state['files_uploaded']:
-        if st.button('Skip Upload', on_click=toggle_closed):
-            st.session_state['files_uploaded'] = True
-            st.success('File Upload Skipped')
+    if not st.session_state["files_uploaded"]:
+        if st.button("Skip Upload", on_click=toggle_closed):
+            st.session_state["files_uploaded"] = True
+            st.success("File Upload Skipped")
 
 
-if st.session_state['files_uploaded']:
-    
-    assistant = create_assistant(client, name=assistant_name, instructions=st.session_state['instructions'], files=st.session_state['files'])
+if st.session_state["files_uploaded"]:
+    assistant = create_assistant(
+        client,
+        name=assistant_name,
+        instructions=st.session_state["instructions"],
+        files=st.session_state["files"],
+    )
 
     thread = create_thread(client)
 
@@ -76,21 +82,16 @@ if st.session_state['files_uploaded']:
             st.markdown(message["content"])
 
     if prompt := st.chat_input("Hey Carbolic, ..."):
-
-        message, log = send_message(client, thread, prompt)
-
         with st.chat_message("user"):
             st.markdown(prompt)
 
+        message, log = send_message(client, thread, prompt)
         st.session_state.messages.append(log)
 
         with st.chat_message("assistant"):
-
             run, log = generate_response(client, thread, assistant)
-
-            message_placeholder = st.empty()
-            message_placeholder.markdown(log['content'])
+            st.markdown(log["content"])
 
         st.session_state.messages.append(log)
 else:
-    st.info('Please upload files or skip upload to proceed.')
+    st.info("Please upload files or skip upload to proceed.")
